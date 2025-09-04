d638f9add8d4ad5c9a66eb4c510061200c694b78
Rakesh singh <rakeshsingh@Rakeshs-MacBook-Pro.local>
2025-09-03 22:54:52 -0600

    removed unused code

--- first-proj/src/app/app.ts
+++ first-proj/src/app/app.ts
@@ -1,24 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-//import {User} from './user/user'
-//import {DataBinding} from './data-binding/data-binding'
-//import {ComponentDirective} from './component-directive/component-directive'
-//import { StructuralDirectives } from './structural-directives/structural-directives';
-//import { AttributeDirectives } from './attribute-directives/attribute-directives';
-import { SignalComponent } from './signal-component/signal-component';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 @Component({
+  standalone:true,
   selector: 'app-root',
   imports: [RouterOutlet,
-    //,User,
-    //DataBinding,
-    //ComponentDirective,
-    //StructuralDirectives,
-    //AttributeDirectives,
-    SignalComponent
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('first-proj');
 }
